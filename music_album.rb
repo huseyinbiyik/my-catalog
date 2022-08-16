23e7dcf27c7aeb05fe2bec127c2f4f7dfc742cb5
bamsi <hibamsi@gmail.com>
2022-08-16 11:48:28 +0300

    create music album class

---
+++ music_album.rb
@@ -0,0 +1,12 @@
+require_relative 'item'
+class MusicAlbum < Item
+  def initialize(*item, on_spotify)
+    genre, author, source, label, publish_date, archived = *item
+    super(genre, author, source, label, publish_date)
+    @on_spotify = on_spotify
+  end
+
+  private
+
+  def can_be_archived?; end
+end
