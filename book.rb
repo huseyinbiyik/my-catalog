daeebde31631886eadbab894454ab378d149934c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 13:45:33 +0430

    solving linters issues

--- book.rb
+++ book.rb
@@ -1,16 +1,16 @@
-require "./item.rb"
+require './item.rb'
 
 class Book < Item
-    def initialize(*item, publisher, cover_state)
-        genre, author, source, label, publish_date, archived = *item
-        super(genre, author, source, label, publish_date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(*item, publisher, cover_state)
+    genre, author, source, label, publish_date, archived = *item
+    super(genre, author, source, label, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    private
+  private
 
-    def can_be_archived?
-        can_be_archived? || @cover_state == "bad"
-    end
+  def can_be_archived?
+    can_be_archived? || @cover_state == 'bad'
+  end
 end
