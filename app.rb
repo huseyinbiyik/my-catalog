334158a5038c3d03c1b04d9dcb7a317af586ad6d
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 20:04:48 +0430

    add a method add_book to recieve the user inputs and add it to the books data

--- app.rb
+++ app.rb
@@ -7,6 +7,26 @@ class App
     @games = []
   end
 
+  def add_book
+    puts 'Enter book title:'
+    title = gets.chomp
+    puts 'Enter book author:'
+    author = gets.chomp
+    puts 'Enter book genre:'
+    genre = gets.chomp
+    puts 'Enter book label:'
+    label = gets.chomp
+    puts 'Enter book source:'
+    source = gets.chomp
+    puts 'Enter publication date:'
+    publish_date = gets.chomp
+    puts 'Enter publisher:'
+    publisher = gets.chomp
+    puts 'Enter book cover state (good, bad):'
+    cover_state = gets.chomp
+    @books << Book.new(genre, author, source, label, publish_date, publisher, cover_state)
+  end
+
   def add_game
     puts 'add game'
   end
