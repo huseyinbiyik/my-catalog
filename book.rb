1e779570749e26b07d86aee80b81188f10e311a2
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 13:25:28 +0430

    solve the parameters issue

--- book.rb
+++ book.rb
@@ -1,9 +1,11 @@
 require "./item.rb"
 
 class Book < Item
-    def initialize(genre, author, source, label, publish_date, publisher, cover_state)
+    def initialize(*item, publisher, cover_state)
+        genre, author, source, label, publish_date, archived = *item
         super(genre, author, source, label, publish_date)
         @publisher = publisher
         @cover_state = cover_state
     end
+
 end
