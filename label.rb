8bc2d9392ed43adcc7435aaa9c13d0f858151e69
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 15:14:54 +0430

    solving linters issues

--- label.rb
+++ label.rb
@@ -1,10 +1,10 @@
 class Label
-    attr_reader :items
-    
-    def initialize(title, color)
-        @id = Random.rand(1..100)
-        @title = title
-        @color = color
-        @items = []
-    end
+  attr_reader :items
+
+  def initialize(title, color)
+    @id = Random.rand(1..100)
+    @title = title
+    @color = color
+    @items = []
+  end
 end
