89ceefff121071edb052424e2f0684f95764705a
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 12:41:07 +0430

    add parameters and super class parameters for Book class

--- book.rb
+++ book.rb
@@ -1,4 +1,9 @@
 require "./item.rb"
 
 class Book < Item
+    def initialize(genre, author, source, label, publish_date, publisher, cover_state)
+        super(genre, author, source, label, publish_date)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
 end
