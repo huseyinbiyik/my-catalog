39a8bc1bb872c468a1b761abb60ad77d336f6874
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-16 14:04:57 +0430

    add the Label class parameters and constructor

--- label.rb
+++ label.rb
@@ -1,4 +1,8 @@
-require './item'
-
 class Label < Item
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
 end
