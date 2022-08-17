df95916d04cb2f184e9b75ec8f517cd37b103be8
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-17 15:44:47 +0430

    add the method to_json to the Book class to parse the json data

--- book.rb
+++ book.rb
@@ -15,4 +15,16 @@ class Book < Item
   def can_be_archived?
     can_be_archived? || @cover_state == 'bad'
   end
+
+  def to_json
+    {
+      'genre' => @genre,
+      'author' => @author,
+      'source' => @source,
+      'label' => @label,
+      'publish_date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state
+    }
+  end
 end
