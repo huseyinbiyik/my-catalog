a2c68795a8141abd2a1cbbdc75aba0213840ca43
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-17 18:07:29 +0430

    created the book table schema

--- databese_sql/schema.sql
+++ databese_sql/schema.sql
@@ -6,3 +6,15 @@ CREATE TABLE item (
     label VARCHAR(255) NOT NULL,
     publish_date DATE NOT NULL,
 )
+
+CREATE TABLE book (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    author VARCHAR(255) NOT NULL,
+    source VARCHAR(255) NOT NULL,
+    label VARCHAR(255) NOT NULL,
+    publish_date DATE NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    cover VARCHAR(255) NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (author, source, label, publish_date) REFERENCES item (author, source, label, publish_date)
+);
