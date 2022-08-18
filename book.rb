aaa775bb52a2ec786124090df297ab0b9921ff4e
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-18 14:12:25 +0430

    change the books class parameters and add data object to the creating book list

--- book.rb
+++ book.rb
@@ -3,9 +3,8 @@ require './item'
 class Book < Item
   attr_accessor :publisher, :cover_state, :publish_date, :author, :source, :genre, :label
 
-  def initialize(*item, publisher, cover_state)
-    genre, author, source, label, publish_date = *item
-    super(genre, author, source, label, publish_date)
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
