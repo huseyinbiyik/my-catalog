b5a1d100929b5521246a82548ebd6ad7774e5737
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-18 16:40:58 +0300

    Add tests for Author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,14 @@
+require './author'
+
+describe Author do
+  context 'instantiate Author class' do
+    author = Author.new('Test_first_name', 'Test_last_name')
+
+    it 'returns first name from the Author object' do
+      expect(author.first_name).to eq 'Test_first_name'
+    end
+    it 'returns first name from the Author object' do
+      expect(author.last_name).to eq 'Test_last_name'
+    end
+  end
+end
