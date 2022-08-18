c9100c58940c3c95f936770cd1b58f93793ed2ce
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-18 19:42:03 +0300

    Add can_be_archived? test for item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require './item'
 
 describe Item do
   before :each do
-    @item = Item.new('2022/12/22')
+    @item = Item.new('2020/12/22')
   end
 
   describe '#new' do
@@ -13,9 +13,16 @@ describe Item do
       -> { Item.new('2022/12/22', '2022/12/22') }.should raise_exception ArgumentError
     end
   end
+
   describe 'publish date' do
     it 'returns publish_date key from the game object' do
       expect(@item.publish_date).to eq '2022/12/22'
+    end
+  end
+
+  describe 'can_be_archived?' do
+    it 'returns false if it older than 10 years' do
+      expect(@item.can_be_archived?).to eq false
     end
   end
 end
